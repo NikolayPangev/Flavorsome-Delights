d2966b6cae5f409d737ee3f52661d273e0604b41
metodieva.n123 <metodieva.n123@gmail.com>
2023-07-12 22:32:49 +0300

    models change

--- flavorsome_delights/Models/Recipe.cs
+++ flavorsome_delights/Models/Recipe.cs
@@ -7,6 +7,11 @@ public class Recipe
 	public Recipe()
 	{
 		public int Id { get; set; }
-
-	}
+		public string title { get; set; }
+		public int user_id { get; set; }
+		public int serves { get; set; }
+		public string howToPrepare { get; set; }
+		public string complexity { get; set; }
+		public string preparationTime { get; set; }
+}
 }
