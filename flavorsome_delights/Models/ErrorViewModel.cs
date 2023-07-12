07db81c5780128f89f87b4b403bcd3b6cde7d1ea
metodieva.n123 <metodieva.n123@gmail.com>
2023-07-12 15:21:12 +0300

    test2

---
+++ flavorsome_delights/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace flavorsome_delights.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
