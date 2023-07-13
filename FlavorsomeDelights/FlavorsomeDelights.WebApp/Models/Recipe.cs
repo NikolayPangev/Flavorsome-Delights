7697ced12869411a2c03565d28c0b9dde27d2514
metodieva.n123 <metodieva.n123@gmail.com>
2023-07-13 15:36:07 +0300

    update info recipe&category

--- FlavorsomeDelights/FlavorsomeDelights.WebApp/Models/Recipe.cs
+++ FlavorsomeDelights/FlavorsomeDelights.WebApp/Models/Recipe.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+//using Microsoft.EntityFrameworkCore;
 //using System.Data.Entity;
 
 namespace FlavorsomeDelights.WebApp.Models
 {
     public class Recipe
     {
-        //[Key]
-        int RecipeId { get; set; }
+        [Key]
+        public int RecipeId { get; set; }
 
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string HowToPrepare { get; set; }
+        [Required]
+        public string Complexity { get; set; }
+        [Required]
+        public string Serves { get; set; }
+        [Required]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
