f35f2b6e7c05e8c8cfcaf01321a77b00d39bf7d2
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-07-13 15:05:47 +0300

    Created Ingredients

---
+++ FlavorsomeDelights/FlavorsomeDelights.WebApp/Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlavorsomeDelights.WebApp.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        public int IngredientId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool isAllergic { get; set; }
+    }
+}
