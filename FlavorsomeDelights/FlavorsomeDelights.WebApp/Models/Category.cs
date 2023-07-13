5f6b0c3741d8a87e1498bb8744889575be16b1ce
metodieva.n123 <metodieva.n123@gmail.com>
2023-07-13 15:52:42 +0300

    update Category

--- FlavorsomeDelights/FlavorsomeDelights.WebApp/Models/Category.cs
+++ FlavorsomeDelights/FlavorsomeDelights.WebApp/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace FlavorsomeDelights.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlavorsomeDelights.WebApp.Models
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Type { get; set; }
     }
 }
