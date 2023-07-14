876ba7e37e37a9f7422e7cdd9841108862e620c8
ralitsa_stancheva <ralitsa_stancheva@abv.bg>
2023-07-14 13:36:22 +0300

    added background-color

--- FlavorsomeDelights/FlavorsomeDelights.WebApp/Database/Contexts.cs
+++ FlavorsomeDelights/FlavorsomeDelights.WebApp/Database/Contexts.cs
@@ -11,6 +11,8 @@ namespace FlavorsomeDelights.WebApp.Database
         public DbSet<Ingredient> Ingredients { get; set; } = null!;
         public DbSet<Recipe> Recipes { get; set; } = null!;
         public DbSet<RecipeIngredient> RecipesIngredients { get; set; } = null!;
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("SqlDb");
