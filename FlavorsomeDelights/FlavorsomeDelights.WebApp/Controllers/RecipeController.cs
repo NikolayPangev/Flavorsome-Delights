e2407b86f59ab20af99b7604433e328c7f40a129
ralitsa_stancheva <ralitsa_stancheva@abv.bg>
2023-07-17 16:54:52 +0300

    front End beginnig

---
+++ FlavorsomeDelights/FlavorsomeDelights.WebApp/Controllers/RecipeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlavorsomeDelights.WebApp.Controllers
+{
+    public class RecipeController : Controller
+    {
+        // GET: Recipe
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Recipe/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Recipe/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipe/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Recipe/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Recipe/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Recipe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Recipe/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
